a9562d9711a20cc3d13ac39a430a4a42b4827ecd
Saldanha Félix Sousa <saldanhafelixsousa91@gmail.com>
2025-11-26 12:12:45 +0100

    o
    url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/reposity_sistema-transacoes-imobiliarias/",
+  base: "/sme/",
   plugins: [react(), tailwindcss(),],
 })
