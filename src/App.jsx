d2af1c235f1f88ea5600220e748e4070ef99e344
Saldanha Félix Sousa <saldanhafelixsousa91@gmail.com>
2025-12-02 03:15:59 +0100

    Trabalhei o carrossel

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,13 @@ import Footer from './components/Footer.jsx'
 import Header from './components/Header.jsx'
 import bgimg_section from './assets/image/img_header/fundo_header.png'
 
-
-import casa1 from './assets/image/img_header/slid_1.png'
-import casa2 from './assets/image/img_header/slid_2.png'
-import casa3 from './assets/image/img_header/slid_3.png'
-import casa4 from './assets/image/img_header/slid_4.png'
 import bg_img from './assets/image/img_header/texto_transparent.png'
 
 
 
 
 import 'swiper/css';
+import EmblaCarousel from './components/EmblaCarousel.jsx'
 
 function App() {
 
@@ -36,54 +32,19 @@ function App() {
 
           </section>
 
-
-
           <section className=" p-2.5 max-sm:mb-7">
-
             <h2 className='bg-amber-400 w-20 my-4 p-1 rounded-[5px]' >Destaque</h2>
 
-            <div id="sub_section2 " className='flex *:basis-[30%] justify-evenly sm:*:h-[300px] *:h-[200px] gap-3.5 *:[&_img]:h-[60%] *:[&_img]:w-full *:[&_img]:bg-black'>
+              <EmblaCarousel/>
+              <EmblaCarousel/>
               
-              <div className=''>
-                <img src={casa1} alt="" />
-                <div id="inf" className='px-2'>
-                  <p>Casa com dois quartos</p>
-                  <p><strong>Arrendamento:</strong> 35.000kz</p>
-                </div>
-              </div>
-
-              <div>
-                <img src={casa2} alt="" />
-                 <div id="inf" className='px-2'>
-                  <p>Casa com dois quartos</p>
-                  <p><strong>Arrendamento:</strong> 50.000kz</p>
-                </div>
-              </div>
-
-              <div>
-                <img src={casa3} alt="" />
-                 <div id="inf" className='px-2'>
-                  <p>Casa com dois quartos</p>
-                  <p><strong>Alugel:</strong> 20.000kz/dia</p>
-                </div>
-              </div>
-
-              <div className=''>
-                <img src={casa4} alt="" />
-                 <div id="inf" className='px-2'>
-                  <p>Casa com dois quartos</p>
-                  <p><strong>Venda:</strong> 90.000.000kz</p>
-                </div>
-              </div>
-            </div>
-           
-
           </section>
 
-          <section className="p-3.5 h-[500px] bg-cover bg-left relative" style={{ backgroundImage: `url(${bg_img})` }}>
+          <section className="p-3.5 h-[500px] bg-cover bg-left relative bg_section3 sm:flex sm:justify-around gap-3.5 sm:bg-gray-100" >
 
+            <img src={bg_img} alt="" />
 
-            <div id="text_section3" className='flex flex-col absolute w-[300px] h-[80%] top-20  right-[10%] bg-white/40 py-3.5 text-center gap-5 rounded-4xl max-sm:w-[55%] max-sm:h-[70%] justify-center'>
+            <div id="text_section3" className='flex flex-col max-sm:absolute w-[300px]  max-sm:top-20  max-sm:right-[10%] bg-white/40 py-3.5 text-center gap-5 rounded-4xl max-sm:w-[55%] max-sm:h-[70%] justify-center'>
 
               <p className='text-[1.5rem]'>
                 Se está a procura de um cubico para arrendar, este artigo é para si. Antes de assinar contrato deve ter alguns cuidados.
---
+++ src/components/EmblaCarousel.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
+
+const EmblaCarousel = () => {
+
+    const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()])
+   return (
+  
+        <div className="embla" ref={emblaRef}>
+          
+          <div className="embla__container *:[&_div]:h-[60%] *:[&_div]:*:w-full *:[&_div]:*:h-full ">
+            <div className="embla__slide ">
+                <div  className='cont_img'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR44D-vqC_gjDOPPAtfnJDp6Au8fTVxcDB-vMeeEn2kYdWakZBD15zciFx2Q54XHm67WrU&usqp=CAU" alt="" />
+                </div>
+
+                <p className='truncate'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p><strong>Localização:</strong></p>
+                <p><strong>Preço:</strong></p>
+            </div>
+
+            <div className="embla__slide">
+                <div className='cont_img'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR44D-vqC_gjDOPPAtfnJDp6Au8fTVxcDB-vMeeEn2kYdWakZBD15zciFx2Q54XHm67WrU&usqp=CAU" alt="" />
+                </div>
+
+                <p className='truncate'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p><strong>Localização:</strong></p>
+                <p><strong>Preço:</strong></p>
+            </div>
+
+            <div className="embla__slide">
+                <div className='cont_img'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPvWbJLoYnTnYTCXS1DWDovmzDFYqYKB8be0YNy9bRd9sneALEDET2qecRSsc_xbOCouw&usqp=CAU" alt="" />
+                </div>
+
+                <p className='truncate'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p><strong>Localização:</strong></p>
+                <p><strong>Preço:</strong></p>
+            </div>
+
+            <div className="embla__slide">
+                <div className='cont_img'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR44D-vqC_gjDOPPAtfnJDp6Au8fTVxcDB-vMeeEn2kYdWakZBD15zciFx2Q54XHm67WrU&usqp=CAU" alt="" />
+                </div>
+
+                <p className='truncate'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p><strong>Localização:</strong></p>
+                <p><strong>Preço:</strong></p>
+            </div>
+
+            <div className="embla__slide">
+                <div className='cont_img'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR44D-vqC_gjDOPPAtfnJDp6Au8fTVxcDB-vMeeEn2kYdWakZBD15zciFx2Q54XHm67WrU&usqp=CAU" alt="" />
+                </div>
+
+                <p className='truncate'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p><strong>Localização:</strong></p>
+                <p><strong>Preço:</strong></p>
+            </div>
+
+            <div className="embla__slide overflow-hidden text-ellipsis whitespace-nowrap ">
+                <div className='cont_img'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR44D-vqC_gjDOPPAtfnJDp6Au8fTVxcDB-vMeeEn2kYdWakZBD15zciFx2Q54XHm67WrU&usqp=CAU" alt="" />
+                </div>
+
+                <p className='truncate'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p><strong>Localização:</strong></p>
+                <p><strong>Preço:</strong></p>
+            </div>
+
+            <div className="embla__slide">
+                <div className='cont_img'>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR44D-vqC_gjDOPPAtfnJDp6Au8fTVxcDB-vMeeEn2kYdWakZBD15zciFx2Q54XHm67WrU&usqp=CAU" alt="" />
+                </div>
+
+                <p className='truncate'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+                <p><strong>Localização:</strong></p>
+                <p><strong>Preço:</strong></p>
+            </div>
+
+          </div>
+        </div>
+  )
+}
+
+export default EmblaCarousel
